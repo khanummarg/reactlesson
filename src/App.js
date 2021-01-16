312c053e7dcf2e7fb554c4aa33333c227c75e3ee
khanummarg <khanummargaryan@gmail.com>
2021-01-16 11:05:55 +0400

    correct directory

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Product from './Product.js'
 // import Todo from './homework-8/Todo.js';
-import Todo from './components/homework-9-11/Todo.js'
+import Todo from './components/ToDo/Todo.js'
 
 // function Greeting(props) {
 //   return (
